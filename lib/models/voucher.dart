89a64dbed866a4c48e3eab631d3b4a35832d6bee
Le Nguyen Anh Khoi <khoi@tiny.fun>
2023-02-19 14:15:18 +0700

    Update Voucher model

--- lib/models/voucher.dart
+++ lib/models/voucher.dart
@@ -1,19 +1,29 @@
 class Voucher {
   final int id;
   final String voucherName;
+  final String voucherCode;
   final String expiredDate;
 
   Voucher(
-      {required this.id, required this.voucherName, required this.expiredDate});
+      {required this.id,
+      required this.voucherName,
+      required this.voucherCode,
+      required this.expiredDate});
 
   Map<String, dynamic> toJson() {
-    return {'id': id, 'voucher_name': voucherName, 'expired_date': expiredDate};
+    return {
+      'id': id,
+      'voucher_name': voucherName,
+      'voucher_code': voucherCode,
+      'expired_date': expiredDate
+    };
   }
 
   static Voucher fromJson(Map<String, dynamic> json) {
     return Voucher(
         id: json['id'],
         voucherName: json['voucher_name'],
+        voucherCode: json['voucher_code'],
         expiredDate: json['expired_date']);
   }
 }
