0c1c9c0c1cce3dc32fed74172ba10f58bb058c4b
LNAKhoi <lnakhoi1811@gmail.com>
2023-02-20 22:46:09 +0700

    Update Auth bloc

--- lib/bloc/auth_bloc.dart
+++ lib/bloc/auth_bloc.dart
@@ -8,8 +8,10 @@ part 'auth_state.dart';
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
   final Auth auth;
   AuthBloc({required this.auth}) : super(UnAuthenticated()) {
-    /*When User Presses the SignIn Button, we will send the SignInRequested Event to the
-     AuthBloc to handle it and emit the Authenticated State if the user is authenticated*/
+    /*
+    When User Presses the SignIn Button, we will send the SignInRequested Event to the
+     AuthBloc to handle it and emit the Authenticated State if the user is authenticated
+     */
 
     on<SignInRequested>((event, emit) async {
       emit(AuthLoading());
