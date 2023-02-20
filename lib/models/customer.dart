98179130df323d0739e36dbb25498e77b356a382
Le Nguyen Anh Khoi <khoi@tiny.fun>
2023-02-21 00:09:06 +0700

    Update Customer model

--- lib/models/customer.dart
+++ lib/models/customer.dart
@@ -1,22 +1,24 @@
 class Customer {
-
   final String id;
   final String email;
   final String birthday;
   final String fullName;
+  final String phoneNumber;
 
   Customer(
       {required this.id,
       required this.email,
       required this.birthday,
-      required this.fullName});
+      required this.fullName,
+      required this.phoneNumber});
 
   Map<String, dynamic> toJson() {
     return {
       'id': id,
       'email': email,
       'birthday': birthday,
-      'full_name': fullName
+      'full_name': fullName,
+      'phone': phoneNumber,
     };
   }
 
@@ -25,6 +27,7 @@ class Customer {
         id: json['id'],
         email: json['email'],
         birthday: json['birthday'],
-        fullName: json['full_name']);
+        fullName: json['full_name'],
+        phoneNumber: json['phone']);
   }
 }
